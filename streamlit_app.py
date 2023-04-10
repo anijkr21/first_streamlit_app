cefe6f4abdd3606524f208a146669735c2fdeeab
anijkr21 <95953730+anijkr21@users.noreply.github.com>
2023-04-10 15:20:58 +0530

    Update streamlit_app.py
    flatenned json format data and modified to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #Just writes the data to the screen
+
+# flattened json format 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# json format to dataframe
+streamlit.dataframe(fruityvice_normalized)
