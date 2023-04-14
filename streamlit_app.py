62b4729bc0def20baddb37ade419eac5bf351ad7
anijkr21 <95953730+anijkr21@users.noreply.github.com>
2023-04-14 15:39:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,19 +25,20 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display FruityVice API
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #Just writes the data to the screen
+except URLError as e:
+  streamlit.error()
 
-# flattened json format 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# json format to dataframe
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
+  streamlit.stop()
 
 #import snowflake.connector
 
